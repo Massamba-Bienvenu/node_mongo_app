a34bc0ddb4a3d889cca0bf29f68bdf8a3275262d
boukamass <boukamass@gmail.com>
2020-11-06 08:22:58 -0600

    test updates

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-const { code } = require("statuses")
+const http = require("http");
+const fs = require("fs");
 
-This file file will hold my server side js code.
+
+const server = http.createServer((req, res)=> {
+    const url = req.url;
+    const method =  req.method;
+    if (url === "/") {
+        res.setHeader("Content-type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>Enter Message</title></head>");
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>');
+        res.write("</html>");
+        return res.end();
+    };
+
+    if (url === "/message" && method === "POST") {
+        fs.writeFileSync("message.text", "DUMMY");
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+    };
+    
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(`<body><h1>Hello from Node.js!!!!</h1></body>`);
+    res.write("</html>");
+    res.end();
+});
+
+server.listen(5000);
