218e5300131abdcb043cb9358146d92b8eea9c90
boukamass <boukamass@gmail.com>
2020-11-10 22:18:09 -0600

    finishing shop app structure

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const path =  require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const adminRoutes = require("./routes/admin.js");
-const shopRoutes = require("./routes/shop.js");
-const errorRoutes = require("./routes/404.js");
+const errorController = require('./controllers/error');
 
-//Setup
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-//Middlewares defintion
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-//404 Catch-all page
-app.use(errorRoutes )
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,19 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
-const shopController = require("../controllers/shop.js");
 
-router.get('/', shopController.getIndex );
+router.get('/', shopController.getIndex);
 
-router.get("/products", shopController.getProducts);
+router.get('/products', shopController.getProducts);
 
-router.get("/cart", shopController.getCart);
+router.get('/cart', shopController.getCart);
 
-router.get("/checkout", shopController.getCheckout);
-  
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
 module.exports = router;
